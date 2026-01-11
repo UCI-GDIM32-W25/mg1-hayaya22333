6de5680d917693283f318881a0f6a1990ac75d4c
Hayaya <hanyang.hayaya@gmail.com>
2026-01-10 19:29:18 -0800

    Coding and devlog

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float _speed = 1.0f;
+    [SerializeField] private float _speed = 5.0f;
     [SerializeField] private Transform _playerTransform;
     [SerializeField] private GameObject _plantPrefab;
     [SerializeField] private int _numSeeds = 5; 
@@ -11,18 +11,37 @@ public class Player : MonoBehaviour
     private int _numSeedsLeft;
     private int _numSeedsPlanted;
 
-    private void Start ()
+    private void Start()
     {
-        
+        _numSeedsLeft = _numSeeds;
+        _numSeedsPlanted = 0;
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
-
     private void Update()
     {
-        
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        Vector3 movement = new Vector3(x, y, 0f).normalized;
+
+        _playerTransform.Translate(movement * _speed * Time.deltaTime);
+
+        if (Input.GetKeyDown("space"))
+        {
+            PlantSeed();
+        }
     }
 
     public void PlantSeed ()
     {
-        
+        if (_numSeedsLeft > 0)
+        {
+            Vector3 _plantPosition = new Vector3(_playerTransform.position.x, _playerTransform.position.y, 1);
+            GameObject plant = Instantiate (_plantPrefab, _plantPosition, _playerTransform.rotation);
+            plant.gameObject.SetActive(true);
+            _numSeedsLeft -= 1;
+            _numSeedsPlanted += 1;
+
+            _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/MjLLqDcN)
 # HW1
 ## Devlog
-Write your Devlog here!
+Han Yang, she/her
 
+
+My completed game project included the 4 object identified in the MG1 break-down activity: seeds, main camera, Player, and UI. the project didn't require any editing on the camera, so my focus was on writing the player and ui scripts and learning to use prefab feature for seeds. I addressed seeds' position attribute by using transform.position in the Player.cs and overloading Instantiate() function to make it appear at player's current location. Since the player object kept integer values for seeds, UpdateSeeds function in PlantCountUI.cs was written as public to allow player's PlantSeed() function to affect the UI displaying numbers of seeds planted/left.
 
 ## Open-Source Assets
 If you added any other outside assets, list them here!
